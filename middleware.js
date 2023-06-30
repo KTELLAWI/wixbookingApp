7ab99f52f756ecc61048b7c40c5daab34cd116af
KTELLAWI <koutaiba@msn.com>
2023-06-30 22:23:19 +0300

    middleware24

---
+++ middleware.js
@@ -0,0 +1,11 @@
+// import { createClient, OAuthStrategy } from '@wix/api-client';
+import{NextResponse} from'next/server'
+
+export async function middleware(request) {
+    if (!request.cookies.get('session')) {
+      const response = NextResponse.next();
+      // const myWixClient = createClient({ auth: OAuthStrategy({ clientId: `27761eb6-dc3e-4cba-910d-602430ceca02` }) });
+      response.cookies.set('session', "JSON.stringify(await myWixClient.auth.generateVisitorTokens())");
+      return response;
+    }
+  }
