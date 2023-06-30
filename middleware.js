beefdeec8b808aafcb4d163467a0a1183dfa6f15
KTELLAWI <koutaiba@msn.com>
2023-06-30 21:38:32 +0300

    middleware17

--- middleware.js
+++ middleware.js
@@ -1,12 +1,27 @@
 import{NextResponse} from'next/server'
 // import process from 'next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss';
-import {myWixClient} from "./helper.js"
+//import {myWixClient} from "./helper.js"
+import { createClient, OAuthStrategy } from '@wix/api-client'
+import { avaiLabiLityCaLender, services } from '@wix/bookings'
+import { redirects } from '@wix/redirects'
+import Cookies from 'js-cookie'
 
 export async function middleware(request){
+    
+
+
+ const myWixClient = createClient({
+    modules: { services, redirects },
+    auth: OAuthStrategy({
+        clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID,
+        tokens: JSON.parse(Cookies.get('session') || null)
+    })
+})
+
     if(!request.cookies.get('session')){
         const response = NextResponse.next();
      
-        response.cookies.set('session',"JSON.stringify(await myWixClient.auth.generateVisitorTokens()"
+        response.cookies.set('session',JSON.stringify(await myWixClient.auth.generateVisitorTokens())
         );
         return response;
 
