3acfd1201abb33140ba2ab9254fd095d77747792
KTELLAWI <koutaiba@msn.com>
2023-06-30 21:46:41 +0300

    middleware19

--- middleware.js
+++ middleware.js
@@ -1,37 +1,11 @@
-import{NextResponse} from'next/server'
-// import process from 'next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss';
-//import {myWixClient} from "./helper.js"
-import { createClient, OAuthStrategy } from '@wix/api-client'
-import { avaiLabiLityCaLender, services } from '@wix/bookings'
-import { redirects } from '@wix/redirects'
-import Cookies from 'js-cookie'
+import { createClient, OAuthStrategy } from '@wix/api-client';
+import { NextResponse } from '/next/server'
 
-export async function middleware(request){
-    
-
-
- const myWixClient = createClient({
-    modules: { services, redirects },
-    auth: OAuthStrategy({
-        clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID,
-        tokens: JSON.parse(Cookies.get('session') || null)
-    })
-})
-
-    if(!request.cookies.get('session')){
-        const response = NextResponse.next();
-     
-        response.cookies.set('session',JSON.stringify(await myWixClient.auth.generateVisitorTokens())
-        );
-        return response;
-
-
+export async function middleware(request) {
+    if (!request.cookies.get('session')) {
+      const response = NextResponse.next();
+      const myWixClient = createClient({ auth: OAuthStrategy({ clientId: `27761eb6-dc3e-4cba-910d-602430ceca02` }) });
+      response.cookies.set('session', JSON.stringify(await myWixClient.auth.generateVisitorTokens()));
+      return response;
     }
-    else{
-        const response = NextResponse.next();
-        return response;
-
-
-
-    }
-}
+  }
