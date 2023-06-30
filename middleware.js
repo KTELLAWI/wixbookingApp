620b38985a7eddecce5c604d97936428c03bed47
KTELLAWI <koutaiba@msn.com>
2023-06-30 21:20:16 +0300

    middleware14

--- middleware.js
+++ middleware.js
@@ -1,12 +1,12 @@
 import{NextResponse} from'next/server'
 // import process from 'next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss';
-// import myWixClient from "./helper.js"
+import myWixClient from "./helper.js"
 
-export  function middleware(request){
+export async function middleware(request){
     if(!request.cookies.get('session')){
         const response = NextResponse.next();
      
-        response.cookies.set('session',"JSON.stringify(await myWixClient.auth.generateVisitorTokens())"
+        response.cookies.set('session',JSON.stringify(await myWixClient.auth.generateVisitorTokens())
         );
         return response;
 
